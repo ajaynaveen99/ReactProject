852a7de8d5c1d05d680d36b6e7cf30e1607484cf
ajaynaveen99 <ajaynaveen337@gmail.com>
2025-12-09 07:17:30 +0530

    vite-project food ordering

---
+++ vite-project/src/User.jsx
@@ -0,0 +1,9 @@
+export default function User() {
+    return (
+        <>
+            <h2>User:isAjay</h2>
+            <button>Login</button>
+            <button>Logout</button>
+        </>
+    )
+}
